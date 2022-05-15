c536745a782a62c5bf96a228e55d4c6446be6222
zdd <zddhub@gmail.com>
2022-05-15 15:21:00 +0800

    Add restaurant card

---
+++ Browse/Restaurant.swift
@@ -0,0 +1,16 @@
+//
+//  Restaurant.swift
+//  Browse
+//
+//  Created by Dongdong Zhang on 2022/5/15.
+//
+
+import Foundation
+
+struct Restaurant {
+  let id: String
+  let image: String
+  let name: String
+  let priceRange: String
+  let description: String
+}
---
+++ Browse/RestaurantCard.swift
@@ -0,0 +1,46 @@
+//
+//  RestaurantCard.swift
+//  Browse
+//
+//  Created by Dongdong Zhang on 2022/5/15.
+//
+
+import SwiftUI
+
+struct RestaurantCard: View {
+  @State var restaurant: Restaurant
+
+  var body: some View {
+    VStack(spacing: 0) {
+      AsyncImage(url: URL(string: restaurant.image)!) { image in
+        image.resizable()
+      } placeholder: {
+        ProgressView()
+      }
+      .frame(height: 260)
+
+      VStack(alignment: .leading, spacing: 8) {
+        HStack {
+          Text("\(restaurant.name)")
+          Spacer()
+          Text("\(restaurant.priceRange)")
+        }
+        .font(.title)
+        Text("\(restaurant.description)")
+          .font(.subheadline)
+      }
+      .padding()
+    }
+  }
+}
+
+struct RestaurantCard_Previews: PreviewProvider {
+  static var previews: some View {
+    RestaurantCard(restaurant: Restaurant(
+      id: "1",
+      image: "https://content.demo.microfrontends.com/images/1-burger.jpg",
+      name: "Becky's Burgers",
+      priceRange: "$$",
+      description: "Juicy burgers, crunchy fries, and creamy shakes"))
+  }
+}
