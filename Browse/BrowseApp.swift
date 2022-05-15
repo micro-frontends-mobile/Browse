f4090ee1c177d02e960390e5e90013bf048db335
zdd <zddhub@gmail.com>
2022-05-15 14:47:51 +0800

    Initial Commit

---
+++ Browse/BrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowseApp.swift
+//  Browse
+//
+//  Created by Dongdong Zhang on 2022/5/15.
+//
+
+import SwiftUI
+
+@main
+struct BrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
