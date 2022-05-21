08eff191ddbe236553325729328a1408fdb3b26f
zdd <zddhub@gmail.com>
2022-05-21 16:08:45 +0800

    Create an example app for browse MFE

---
+++ BrowseExample/BrowseExample/BrowseExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowseExampleApp.swift
+//  BrowseExample
+//
+//  Created by Dongdong Zhang on 2022/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BrowseExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
